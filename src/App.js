342469f8cd16af06cb4e22ee69604340eeb015a0
VanshArora08 <kumar.vansharora@gmail.com>
2023-08-05 23:52:54 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Restorant from "./pages/Restorant";
 import Products from "./pages/Products";
+import Navbar from "./components/NavBar";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route exact path="/" element={<LandingPage />}></Route>
         <Route exact path="/restorant" element={<Restorant />}></Route>
