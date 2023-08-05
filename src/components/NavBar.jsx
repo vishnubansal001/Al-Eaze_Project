fe96f002fb275f088a31d6b8c640fe0bc7999a45
VanshArora08 <kumar.vansharora@gmail.com>
2023-08-06 00:03:35 +0530

    updated navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
       </div>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/products">Recommendation</Link></li>
+        <li><Link to="/consultants">Consultants</Link></li>
+        <li><Link to="/blogs">Blog</Link></li>
       </ul>
       <div className="navbar-profile">
         <FaUser size={24} />
